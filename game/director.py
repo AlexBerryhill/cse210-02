24c340eb5dc0daf648599fa898c13ef09fcea759
Alexander Berryhill <amsb99@gmail.com>
2022-01-20 07:57:26 -0700

    Created the classes

---
+++ game/director.py
@@ -0,0 +1,14 @@
+from game.player import Player
+
+class Director:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        pass
+    
+    def start_game():
+        """
+        """
+        pass
